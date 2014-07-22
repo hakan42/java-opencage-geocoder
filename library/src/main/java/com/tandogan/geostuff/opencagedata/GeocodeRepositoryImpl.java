42a348d359f5242974e2616b0198aded759325bf
Hakan Tandogan <hakan@gurkensalat.com>
2014-07-22 19:46:58 +0200

    WIP

--- library/src/main/java/com/tandogan/geostuff/opencagedata/GeocodeRepositoryImpl.java
+++ library/src/main/java/com/tandogan/geostuff/opencagedata/GeocodeRepositoryImpl.java
@@ -1,11 +1,59 @@
 package com.tandogan.geostuff.opencagedata;
 
 import com.tandogan.geostuff.opencagedata.entity.GeocodeResponse;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+
+@Repository
 public class GeocodeRepositoryImpl implements GeocodeRepository
 {
+    private final static String API_KEY = "key";
+
+    private final static String QUERY = "q";
+
+    private String apiKey;
+
+    private String urlBase;
+
+    private RestOperations template;
+
+    public GeocodeRepositoryImpl(RestOperations template)
+    {
+        this.template = template;
+    }
+
+    public String getApiKey()
+    {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey)
+    {
+        this.apiKey = apiKey;
+    }
+
+    public String getUrlBase()
+    {
+        return urlBase;
+    }
+
+    public void setUrlBase(String urlBase)
+    {
+        this.urlBase = urlBase;
+    }
+
     public GeocodeResponse query(String query)
     {
-        return null;
+        URI serviceUrl = UriComponentsBuilder.fromUriString(getUrlBase())
+                .queryParam(API_KEY, apiKey)
+                .queryParam(QUERY, query)
+                .build()
+                .toUri();
+
+        GeocodeResponse result = template.getForObject(serviceUrl, GeocodeResponse.class);
+        return result;
     }
 }
--- library/src/test/java/com/tandogan/geostuff/opencagedata/GeocodeRepositoryTest.java
+++ library/src/test/java/com/tandogan/geostuff/opencagedata/GeocodeRepositoryTest.java
@@ -1,22 +1,32 @@
 package com.tandogan.geostuff.opencagedata;
 
+import com.tandogan.geostuff.opencagedata.entity.EntityComponentScanMarker;
 import com.tandogan.geostuff.opencagedata.entity.GeocodeResponse;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestOperations;
 
 import static org.junit.Assert.assertNull;
 
+@ComponentScan(basePackageClasses = {EntityComponentScanMarker.class, RepositoryComponentScanMarker.class})
 public class GeocodeRepositoryTest
 {
-    GeocodeRepository testable;
+    @Mock
+    RestOperations restTemplate;
+
+    GeocodeRepositoryImpl testable;
 
     @Before
     public void setUp()
     {
         MockitoAnnotations.initMocks(this);
 
-        testable = new GeocodeRepositoryImpl();
+        testable = new GeocodeRepositoryImpl(restTemplate);
+        testable.setApiKey("4711");
+        testable.setUrlBase("http://api.opencagedata.com/geocode/v1/json");
     }
 
     @Test
