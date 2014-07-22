16bca6a07aa48bef54ad9049420b329c744fea7c
Hakan Tandogan <hakan@gurkensalat.com>
2014-07-23 00:38:14 +0200

    Use MockRestServiceServer

--- library/src/test/java/com/tandogan/geostuff/opencagedata/GeocodeRepositoryImplTest.java
+++ library/src/test/java/com/tandogan/geostuff/opencagedata/GeocodeRepositoryImplTest.java
@@ -9,17 +9,21 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.web.client.RestOperations;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertNull;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @ComponentScan(basePackageClasses = {EntityComponentScanMarker.class, RepositoryComponentScanMarker.class})
 @PropertySources({
@@ -29,27 +33,37 @@ import static org.junit.Assert.assertNull;
 @RunWith(MockitoJUnitRunner.class)
 public class GeocodeRepositoryImplTest
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeocodeRepositoryImplTest.class);
+
     @Autowired
     private Environment env;
 
     @Mock
-    RestOperations restTemplate;
+    RestTemplate restTemplate;
 
     @InjectMocks
     GeocodeRepositoryImpl testable;
+
+    MockRestServiceServer mockServer;
 
     @Before
     public void setUp()
     {
         MockitoAnnotations.initMocks(this);
 
-        testable.setApiKey(env.getProperty("${API_KEY}"));
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+
+        testable.setApiKey("foobar-4711");
         testable.setUrlBase("http://api.opencagedata.com/geocode/v1/json");
     }
 
     @Test
     public void testFoo()
     {
+        String data = "{ \"id\" : \"42\", \"name\" : \"Holiday Inn\"}";
+        // TODO the mockServer should actually respond to any urls
+        mockServer.expect(method(HttpMethod.GET)).andRespond(withSuccess(data, MediaType.APPLICATION_JSON));
+
         GeocodeResponse result = testable.query("foo");
         assertNull(result);
     }
