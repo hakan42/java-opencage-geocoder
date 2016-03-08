e95a4ce0f7a7553f1025027b154c807b3eaf995a
Hakan Tandogan <hakan@tandogan.com>
2016-03-08 20:46:52 +0100

    Make script more robust against query changes

--- update_test_data.sh
+++ update_test_data.sh
@@ -1,9 +1,16 @@
 #! /bin/sh -x
 
-$(dirname $0)/geocode.sh germering > $(dirname $0)/library/src/test/resources/de-by-germering.json
-$(dirname $0)/geocode.sh kas       > $(dirname $0)/library/src/test/resources/tr-antalya-kas.json
-$(dirname $0)/geocode.sh serik     > $(dirname $0)/library/src/test/resources/tr-antalya-serik.json
-$(dirname $0)/geocode.sh keciborlu > $(dirname $0)/library/src/test/resources/tr-isparta-keciborlu.json
+$(dirname $0)/geocode.sh germering \
+	     > $(dirname $0)/library/src/test/resources/de-by-germering.json
+
+$(dirname $0)/geocode.sh kas,antalya \
+	     > $(dirname $0)/library/src/test/resources/tr-antalya-kas.json
+
+$(dirname $0)/geocode.sh serik \
+	     > $(dirname $0)/library/src/test/resources/tr-antalya-serik.json
+
+$(dirname $0)/geocode.sh keciborlu \
+	     > $(dirname $0)/library/src/test/resources/tr-isparta-keciborlu.json
 
 $(dirname $0)/pretty_print_test_data.sh
 
