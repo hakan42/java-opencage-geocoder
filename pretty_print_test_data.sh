c8f2a5a43913bf7ac2194764eeb351f2e6f323db
Hakan Tandogan <hakan@tandogan.com>
2016-03-25 22:02:16 +0100

    Update the reverse geocoding data as well

--- pretty_print_test_data.sh
+++ pretty_print_test_data.sh
@@ -1,6 +1,8 @@
 #! /bin/sh -x
 
-for d in de-by-germering tr-antalya-kas tr-antalya-serik tr-isparta-keciborlu
+for d in \
+    de-by-germering tr-antalya-kas tr-antalya-serik tr-isparta-keciborlu \
+    br--1.05--46.76 cn-+30.63-+122.07 de-+53.05-+8.73 it-+42.01-+12.50 jp-+35.69-+139.78
 do
 
     python -m json.tool < $(dirname $0)/library/src/test/resources/${d}.json > $(dirname $0)/library/src/test/resources/${d}-pretty.json
--- update_test_data.sh
+++ update_test_data.sh
@@ -12,5 +12,20 @@ $(dirname $0)/geocode.sh serik \
 $(dirname $0)/geocode.sh keciborlu \
 	     > $(dirname $0)/library/src/test/resources/tr-isparta-keciborlu.json
 
+$(dirname $0)/geocode.sh -1.05+-46.76 \
+	     > $(dirname $0)/library/src/test/resources/br--1.05--46.76.json
+
+$(dirname $0)/geocode.sh +30.63-+122.07 \
+	     > $(dirname $0)/library/src/test/resources/cn-+30.63-+122.07.json
+
+$(dirname $0)/geocode.sh +53.05-+8.73 \
+	     > $(dirname $0)/library/src/test/resources/de-+53.05-+8.73.json
+
+$(dirname $0)/geocode.sh +42.01-+12.50 \
+	     > $(dirname $0)/library/src/test/resources/it-+42.01-+12.50.json
+
+$(dirname $0)/geocode.sh +35.69-+139.78 \
+	     > $(dirname $0)/library/src/test/resources/jp-+35.69-+139.78.json
+
 $(dirname $0)/pretty_print_test_data.sh
 
