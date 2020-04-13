0a5859f83ea75245ea2e03881cc02fee47387468
Hakan Tandogan <hakan@tandogan.com>
2020-04-13 21:33:25 +0200

    Use jq for json handling
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- pretty_print_test_data.sh
+++ pretty_print_test_data.sh
@@ -5,5 +5,5 @@ for d in \
     br--1.05--46.76 cn-+30.63-+122.07 de-+53.05-+8.73 it-+42.01-+12.50 jp-+35.69-+139.78
 do
 
-    python -m json.tool < $(dirname $0)/library/src/test/resources/${d}.json > $(dirname $0)/library/src/test/resources/${d}-pretty.json
+    jq "." < $(dirname $0)/library/src/test/resources/${d}.json > $(dirname $0)/library/src/test/resources/${d}-pretty.json
 done
